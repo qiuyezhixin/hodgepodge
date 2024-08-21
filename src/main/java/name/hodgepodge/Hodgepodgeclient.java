f9b79bb39a58fac88784a9ecf020e617a86b4f58
qiu_ye_zhi_xin <swnotma@outlook.com>
2024-08-21 17:22:26 +0800

    Initial commit

---
+++ src/main/java/name/hodgepodge/Hodgepodgeclient.java
@@ -0,0 +1,10 @@
+package name.hodgepodge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Hodgepodgeclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
